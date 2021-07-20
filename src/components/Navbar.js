65807388933bb76df3754549c3eae3a4d9544d35
Harshit Bhanushali <harshit.21810372@viit.ac.in>
2021-07-20 16:32:08 +0530

    Complete Navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,29 @@
 import React from 'react'
+import logo from '../logo.svg';
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/#">Navbar</a>
+                <a className="navbar-brand me-5" href="/#"><img src={logo} alt="logo" style={{ width: "35px" }} /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav m-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/#">Home</a>
+                            <a className="nav-link active text-white text-uppercase me-5" aria-current="page" href="/#">Home&nbsp; <i class="fas fa-home"></i></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="/#">Action</a></li>
-                                <li><a className="dropdown-item" href="/#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="/#">Something else here</a></li>
-                            </ul>
+                            <a className="nav-link text-white text-uppercase me-5" href="/#">News</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="/#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <a className="nav-link text-white text-uppercase me-5" href="/#">Contact Us</a>
                         </li>
                     </ul>
                     <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
                     </form>
                 </div>
             </div>
